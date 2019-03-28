f63a6ed376d0cbce106db69bfec49e6939529beb
Maximilian Ruta <mr@xtain.net>
2019-03-28 13:21:56 +0100

    first commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace XTAIN\PatchBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+	/**
+	 * Generates the configuration tree.
+	 *
+	 * @return TreeBuilder
+	 */
+	public function getConfigTreeBuilder()
+	{
+		$treeBuilder = new TreeBuilder();
+		$rootNode = $treeBuilder->root('xtain_patch', 'array');
+
+		return $treeBuilder;
+	}
+}
---
+++ XTAINPatchBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace XTAIN\PatchBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class XTAINPatchBundle extends Bundle
+{
+}
