34ddb0336d0bea4a51f8408a2c07b4a587348332
Maximilian Ruta <mr@xtain.net>
2019-03-28 16:29:33 +0100

    add editorconfig

--- DependencyInjection/XTAINPatchExtension.php
+++ DependencyInjection/XTAINPatchExtension.php
@@ -18,7 +18,7 @@ class XTAINPatchExtension extends Extension
 		$configuration = new Configuration();
 		$config = $this->processConfiguration($configuration, $configs);
 
-		$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+		$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 		$loader->load('services.yaml');
 	}
 }
